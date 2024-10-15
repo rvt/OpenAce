f5ad0926666de7a55f3c4cc3c08e2b1b92cd9adc
R. van Twisk <github@rvt.dds.nl>
2024-10-15 20:58:55 +0200

    open_ace module don't exit in mock/pico.h

--- src/lib/mocks/pico.h
+++ src/lib/mocks/pico.h
@@ -11,12 +11,10 @@ typedef unsigned int uint;
 inline void panic_unsupported(const char *file, int line, const char *func, const char *msg)
 {
     printf("panic_unsupported: %s:%d %s: %s\n", file, line, func, msg);
-    exit(1);
 }
 
 
 inline void panic(const char *msg)
 {
-    printf("%s\n", msg);
-    exit(1);
+    printf("panic: %s\n", msg);
 }
---
+++ src/lib/openace_module.cmake
@@ -0,0 +1,40 @@
+# common_module.cmake
+set(MSG_PREFIX "${PROJECT_NAME} |")
+message("### Including ${CMAKE_CURRENT_LIST_DIR}/${PROJECT_NAME}")
+
+option(BUILD_TESTS "Build unit tests" OFF)
+option(NO_STL "No STL" OFF)
+
+# Create the library
+add_library(${PROJECT_NAME} STATIC ${MODULE_SOURCE_FILES})
+
+# Include directories
+target_include_directories(${PROJECT_NAME} INTERFACE
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+    PRIVATE $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/../include>
+)
+
+# Set compile options
+set_source_files_properties(${MODULE_SOURCE_FILES} PROPERTIES
+    COMPILE_FLAGS "-Wall -Wextra -Werror"
+)
+
+
+foreach(PIO_FILENAME ${MODULE_PIO_FILES})
+    pico_generate_pio_header(${PROJECT_NAME} ${PIO_FILENAME})
+endforeach()
+
+# Since core our own project that uses this, we don't add it when it'c core
+if (NOT "${PROJECT_NAME}" STREQUAL "core")
+    set(EXTRA_LIBS core)
+endif()
+
+# Link libraries
+target_link_libraries(${PROJECT_NAME} 
+    PRIVATE
+    FreeRTOS-Kernel-Heap4
+    pico_stdlib
+    etl::etl
+    ${EXTRA_LIBS}
+    ${MODULE_TARGET_LINK}
+)
